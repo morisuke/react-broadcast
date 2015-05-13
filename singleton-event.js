7eee443cb463fa0f5e07ca6718b0880d64b4d4d7
root <root@vagrant-centos65.vagrantup.com>
2015-05-13 18:06:35 +0000

    indent

--- singleton-event.js
+++ singleton-event.js
@@ -6,17 +6,16 @@ class SingletonEvent extends Event.EventEmitter
 {
 	constructor(enforcer) {
 		super();
-
-        if ( enforcer != SINGLETON_ENFORCER ) {
-            throw new Error( 'Cannot construct singleton' );
-        }
+		if ( enforcer != SINGLETON_ENFORCER ) {
+			throw new Error( 'Cannot construct singleton' );
+		}
 	}
 
 	static get instance() {
 		if (! (this[ SINGLETON_PROP ])) {
-            this[ SINGLETON_PROP ] = new SingletonEvent( SINGLETON_ENFORCER );
-        }
-        return this[ SINGLETON_PROP ];
+			this[ SINGLETON_PROP ] = new SingletonEvent( SINGLETON_ENFORCER );
+		}
+		return this[ SINGLETON_PROP ];
 	}
 	
 	broadcast(event, callback) {
